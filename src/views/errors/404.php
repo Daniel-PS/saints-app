69c038cbd40b175740a3116f996934429e8cc68a
Daniel Pereira <daniel@marcasite.com.br>
2021-08-16 22:17:15 -0300

    Add layout to 404 page

--- src/views/errors/404.php
+++ src/views/errors/404.php
@@ -1,6 +1,26 @@
-<?php 
+<?php
     view('partials/header.php');
 ?>
-    <h1>404!</h1>
-</body>
-</html>
+<section id="hero">
+    <div class="container" style="margin-top: 150px;">
+        <div class="row justify-content-center">
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <img src="<?= BASE_URL ?>images/saint_anthony.jpg" alt="service" class="icon" style="width: 500px; height: 300px;">
+                        <h5 class="card-title" style="font-size: 50pt;">
+                            404
+                        </h5>
+                        <h4>Page not found</h4>
+                        <p class="card-text">
+                            Dear St. Anthony, Please come around. This page is lost and cannot be found
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+    view('partials/footer.php');
+?>
