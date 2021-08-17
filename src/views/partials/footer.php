86d36ea7860e6d82455ce9ffa7009634b9596e25
Daniel Pereira <daniel@marcasite.com.br>
2021-08-16 21:18:44 -0300

    Add header and footer layout

---
+++ src/views/partials/footer.php
@@ -0,0 +1,12 @@
+    <section id="contact">
+        <div class="container">
+            <div class="row align-items-center projects">
+                <div class="col">
+                    <h1>Omnes enim Christus, nihil sine Maria</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aliquid voluptatem autem porro animi rem cumque facere non officia excepturi, totam voluptatibus architecto ipsam. Sunt amet esse nisi voluptatem distinctio.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -2,8 +2,42 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script defer src="<?= BASE_URL ?>js/main.js"></script>
+    <script defer src="<?= BASE_URL ?>js/jquery.mask.min.js"></script>
     <title>Saints</title>
 </head>
 <body>
+    <!-- NAVBAR -->
+    <nav class="py-3 navbar navbar-expand-lg fixed-top auto-hiding-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?= BASE_URL ?>images/logo.png" class="logo" alt="logo">
+                Saints
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Saints</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- NAVBAR END-->
