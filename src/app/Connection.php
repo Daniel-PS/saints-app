3332bf81005a19e114873e09ac71be58878c57ed
Daniel Pereira <daniel@marcasite.com.br>
2021-08-18 00:18:38 -0300

    Correct enviroment username

--- src/app/Connection.php
+++ src/app/Connection.php
@@ -10,7 +10,7 @@ class Connection
     {
         $dbHost = getenv('DB_HOST');
         $dbName = getenv('DB_DATABASE');
-        $dbUser = getenv('DB_USER');
+        $dbUser = getenv('DB_USERNAME');
         $dbPassword = getenv('DB_PASSWORD');
 
         return new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
