1436d451f8cf4086902a7262b5572aab40324e65
Daniel Pereira <daniel@marcasite.com.br>
2021-08-19 21:02:34 -0300

    Edit database

--- docker-compose/mysql/database.sql
+++ docker-compose/mysql/database.sql
@@ -1,13 +1,72 @@
-
--- Qualquer arquivo .sql que estiver nessa pasta vai ser executado no bdd sempre que vc fizer um "up"
-
-CREATE TABLE users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `user_types` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `type` varchar(60),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (name) VALUES ('Daniel');
-INSERT INTO users (name) VALUES ('Gabriel');
-INSERT INTO users (name) VALUES ('Ieda');
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_type_id` int DEFAULT 2,
+  `photo` varchar(60),
+  `name` varchar(60),
+  `surname` varchar(60),
+  `email` varchar(60),
+  `password` varchar(60),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `saints` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `photo` varchar(60),
+  `name` varchar(60),
+  `baptism_name` varchar(60),
+  `birthdate` date,
+  `feast_date` date,
+  `nation` varchar(60),
+  `city` varchar(60),
+  `phrase` longtext,
+  `bio` longtext,
+  `prayer` longtext,
+  `approved` tinyint DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `comments` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `saint_id` int,
+  `comment` longtext,
+  `approved` tinyint,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `users_devotion_saints` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `saint_id` int,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE saints_db.users ADD CONSTRAINT users_FK FOREIGN KEY (user_type_id) REFERENCES saints_db.user_types(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE saints_db.users_devotion_saints ADD CONSTRAINT users_devotion_saints_FK FOREIGN KEY (saint_id) REFERENCES saints_db.saints(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE saints_db.users_devotion_saints ADD CONSTRAINT users_devotion_saints_FK_1 FOREIGN KEY (user_id) REFERENCES saints_db.users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE saints_db.saints ADD CONSTRAINT saints_FK FOREIGN KEY (user_id) REFERENCES saints_db.users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE saints_db.comments ADD CONSTRAINT comments_FK FOREIGN KEY (user_id) REFERENCES saints_db.users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE saints_db.comments ADD CONSTRAINT comments_FK_1 FOREIGN KEY (saint_id) REFERENCES saints_db.saints(id) ON UPDATE CASCADE;
+
+INSERT INTO saints_db.user_types (`type`, created_at, updated_at) VALUES ('Admin', NULL, NULL),	 ('Normal', NULL, NULL);
+
+INSERT INTO saints_db.user_types (type) VALUES ('Admin'), ('Normal');
+
+INSERT INTO saints_db.users (user_type_id, photo, name, surname, email, password, created_at, updated_at) VALUES (1, NULL, 'Daniel', 'Pereira', 'daniel.per.s@hotmail.com', '$2y$10$qQjKC7g.YgX8jyDGVqxoBuf8i/zpDZo8oVSvdcgEzKhn62MDI60Zy', NULL, NULL);
