c2a8bb35edb012ce19f000fd25166fcc2ec23d73
Daniel Pereira <daniel@marcasite.com.br>
2021-08-21 13:16:14 -0300

    Add good samaritan page

---
+++ src/views/goodSamaritan.php
@@ -0,0 +1,50 @@
+<?php
+    view('partials/header.php');
+?>
+<section >
+    <div class="container-fluid h-custom good-samaritan" style="color: white; padding: 0;">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+            a
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 col-lg-12 col-xl-12 align-items-justify">
+        <section id="hero" >
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>The Good Samaritan</h1>
+                    </div>
+                    <p class="phrase" style="font-size: 14pt; text-align: justify;">
+                        &nbsp;&nbsp;&nbsp;&nbsp; The parable of the Good Samaritan is told by Jesus in the Gospel of Luke.[Lk 10:25–37] It is about a traveler who is stripped of clothing, beaten, and left half dead alongside the road. First a Jewish priest and then a Levite comes by, but both avoid the man. Finally, a Samaritan happens upon the traveler. Although Samaritans and Jews despised each other, the Samaritan helps the injured man. Jesus is described as telling the parable in response to the question from a lawyer, "And who is my neighbor?" The conclusion is that the neighbor figure in the parable is the one who shows mercy to the injured fellow man—that is, the Samaritan.
+                        <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp; Saint Augustine has interpreted the parable allegorically, with the Samaritan representing Our Lord Jesus Christ, who saves the sinful soul. The parable has inspired painting, sculpture, poetry, photography, and film. The phrase "Good Samaritan" now has connotation of someone who helps a stranger. Many hospitals and charitable organizations are named after the Good Samaritan.
+                        <br>
+                        <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp; In The case of this website, however, the Good Samaritan is someone who contributed registering a Saint but did not register his authorship; a true example of humility.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="hero">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>Saint Augustine's commentary about this parable</h1>
+                    </div>
+                    <p class="phrase" style="font-size: 15pt; font-style: italic; text-align: justify;">
+                        &nbsp;&nbsp;&nbsp;&nbsp; "A certain man went down from Jerusalem to Jericho;  Adam himself is meant; Jerusalem is the heavenly city of peace, from whose blessedness Adam fell; Jericho means the moon, and signifies our mortality, because it is born, waxes, wanes, an dies. Thieves are the devil and his angels. Who stripped him, namely; of his immortality; and beat him, by persuading him to sin; and left him half-dead, because in so far as man can understand and know God, he lives, but in so far as he is wasted and oppressed by sin, he is dead; he is therefore called half-dead.  The priest and the Levite who saw him and passed by, signify the priesthood and ministry of the Old Testament which could profit nothing for salvation. Samaritan means Guardian, and therefore the Lord Himself is signified by this name. The binding of the wounds is the restraint of sin. Oil is the comfort of good hope; wine the exhortation to work with fervent spirit. The beast is the flesh in which He deigned to come to us. The being set upon the beast is belief in the incarnation of Christ. The inn is the Church, where travelers returning to their heavenly country are refreshed after pilgrimage. The morrow is after the resurrection of the Lord. The two pence are either the two precepts of love, or the promise of this life and of that which is to come. The innkeeper is the Apostle (Paul). The supererogatory payment is either his counsel of celibacy, or the fact that he worked with his own hands lest he should be a burden to any of the weaker brethren when the Gospel was new, though it was lawful for him “to live by the gospel”".
+                        <br>
+                        <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp; &#8212; St. Augustine, Quaestiones Evangeliorum, II, 19
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+
+<?php
+    view('partials/footer.php');
+?>
